be0bbac0712cd65a89171cfa28040c020ca1ea8b
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2022-12-18 13:33:48 -0600

    Estado de categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react"
 
 export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['Code geass']);
     return (
         <>
             <h1>GifExpertApp</h1>
+            <ol>
+                {categories.map((category, i) => <li key={i}>{category}</li>)}
+            </ol>
         </>
     )
 }

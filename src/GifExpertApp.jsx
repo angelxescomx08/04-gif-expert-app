2add13eab66f9a1c09e6e74042087529733a5f38
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2022-12-19 21:07:49 -0600

    Peticiones para los gifs

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react"
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
     const [categories, setCategories] = useState(['Code geass']);
 
     const onAddCategory = (valor) => {
+        if (categories.includes(valor)) return;
         setCategories(categories => [valor, ...categories])
     }
 
@@ -12,9 +14,13 @@ export const GifExpertApp = () => {
         <>
             <h1>GifExpertApp</h1>
             <AddCategory onAddCategory={onAddCategory} />
-            <ol>
-                {categories.map((category, i) => <li key={i}>{category}</li>)}
-            </ol>
+
+            {categories.map((category) => (
+                <GifGrid
+                    key={category}
+                    category={category}
+                />
+            ))}
         </>
     )
 }
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,13 @@
+export const getGif = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=rn5B6tqtKopC8pWpWLboyXWe4zhvrnbl&q=${category}&limit=20`
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium
+    }))
+
+    return gifs;
+}

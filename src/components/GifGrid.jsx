41de8cbe4260fb3174b0ca2c885aaa22e16bf21e
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2022-12-20 20:17:04 -0600

    GifItem

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,15 +1,33 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { getGif } from "../helpers/getGifs"
+import { GridItem } from "./GridItem";
 
 export const GifGrid = ({ category }) => {
 
-    useEffect(()=>{
-        getGif(category)
-    },[])
+    const [images, setImages] = useState([]);
+
+    const getImages = async () => {
+        const newImages = await getGif(category);
+        setImages(newImages);
+    }
+
+    useEffect(() => {
+        getImages();
+    }, [])
 
     return (
         <>
             <h3>{category}</h3>
+            <div className="card-grid">
+                {
+                    images.map((image) => (
+                        <GridItem 
+                            key={image.id} 
+                            {...image}
+                        />
+                    ))
+                }
+            </div>
         </>
     )
 }
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -6,8 +6,7 @@ export const getGif = async (category) => {
     const gifs = data.map(img => ({
         id: img.id,
         title: img.title,
-        url: img.images.downsized_medium
+        url: img.images.downsized_medium.url
     }))
-
     return gifs;
 }
